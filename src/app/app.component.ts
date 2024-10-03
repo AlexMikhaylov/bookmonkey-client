ac55083027dc44f497d4f4f45fcb3d5c21022e5a
AlexMikhaylov <alx.mikhaylov@gmail.com>
2024-10-03 23:13:31 +0200

    Add initial components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+// decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookmonkey-client';
+
+  constructor() {
+    setTimeout(() => {
+      this.title = 'Alex'
+    }, 3000)
+  }
 }
